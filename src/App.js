df6d1a77b1b4ed8856aa6640ea3cae72333e5203
theo886 <theo886@gmail.com>
2025-03-03 15:49:37 -0800

    database add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import WeeklyPercentageTracker from './components/WeeklyPercentageTracker';
+import DatabaseTest from './components/DatabaseTest';
 
 function App() {
   return (
     <div className="min-h-screen py-8 bg-slate-100">
-      <main>
+      <main className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold text-center mb-8">Weekly Time Allocation</h1>
+        
+        {/* Database Test Component */}
+        <div className="mb-8">
+          <DatabaseTest />
+        </div>
+        
+        {/* Main Application Component */}
         <WeeklyPercentageTracker />
       </main>
     </div>
